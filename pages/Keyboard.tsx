258614f1ed9c1f11adae3006128904ab5ba8a0f1
Yana Eva Gonchar <johanna.gonchar@icloud.com>
2023-06-29 14:46:59 +0300

    Player with media comtrollers

---
+++ pages/Keyboard.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import {Block} from '../styled/Block';
+import {Text} from '../styled/Text';
+
+const Keyboard = () => {
+  return (
+    <Block>
+      <Text> Keyboard Avoiding View</Text>
+    </Block>
+  );
+};
+
+export default Keyboard;
