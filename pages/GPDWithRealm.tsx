83cd06bb868d6fabcf4589a472ea6ea365b91086
Yana Eva Gonchar <johanna.gonchar@icloud.com>
2023-06-30 13:58:33 +0300

    Initial commit

---
+++ pages/GPDWithRealm.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import {Block} from '../styled/Block';
+import {Text} from '../styled/Text';
+
+const GPDWithRealm = () => {
+  return (
+    <Block>
+      <Text>GPS with Realm</Text>
+    </Block>
+  );
+};
+
+export default GPDWithRealm;
